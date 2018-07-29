545c7013a39dee09e9d8fbf83abd0e270f2fe914
gertga <gertga@gertga-1.ger.SAS.com>
2018-07-29 17:11:37 +0200

    Spielerprofil angepasst
    Getter/Setter hinzugefügt
    Konstruktor nur für name(alter sollte gelöscht werden da Spielerprofil
    mit 0 spielen etc. generiert wird)
    Durchschnittliche Anzahl Züge hinzugefügt

--- src/Spielerprofil.java
+++ src/Spielerprofil.java
@@ -5,7 +5,7 @@ public class Spielerprofil {
 	public int anzahlSpiele;
 	public int gewonneneSpiele;
 	public int verloreneSpiele;
-
+	public int durchschnittlicheZüge;
 	
 	public Spielerprofil(String spielername, int anzSpiele, int gewSpiele, int verSpiele) {
 		
@@ -15,16 +15,63 @@ public class Spielerprofil {
 		verloreneSpiele = verSpiele;
 	}
 	
+	public Spielerprofil(String spielername) {
+		name = spielername;
+	}
+	
 	public void print() {
 		
 		System.out.println("Spielername: " + name);
 		System.out.println("Anzahl Spiele: " + anzahlSpiele);
 		System.out.println("Gewonnene Spiele: " + gewonneneSpiele);
 		System.out.println("Verlorene Spiele: " + verloreneSpiele);
+		System.out.println("Die durchschnittliche Anzahl an Zügen ist:" + durchschnittlicheZüge);
 	}
 	
 	public static void main (String[]args) {
 		
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAnzahlSpiele() {
+		return anzahlSpiele;
+	}
+
+	public void setAnzahlSpiele(int anzahlSpiele) {
+		this.anzahlSpiele = anzahlSpiele;
+	}
+
+	public int getGewonneneSpiele() {
+		return gewonneneSpiele;
+	}
+
+	public void setGewonneneSpiele(int gewonneneSpiele) {
+		this.gewonneneSpiele = gewonneneSpiele;
+	}
+
+	public int getVerloreneSpiele() {
+		return verloreneSpiele;
+	}
+
+	public void setVerloreneSpiele(int verloreneSpiele) {
+		this.verloreneSpiele = verloreneSpiele;
+	}
+
+	public int getDurchschnittlicheZüge() {
+		return durchschnittlicheZüge;
+	}
+
+	public void setDurchschnittlicheZüge(int durchschnittlicheZüge) {
+		this.durchschnittlicheZüge = durchschnittlicheZüge;
+	}
+	
+	
 	
 }
