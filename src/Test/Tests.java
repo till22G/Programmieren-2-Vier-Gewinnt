25aaeaf05370d34659a3641c8b2fe6330e8be0e7
Max Schneider <m4x1337@icloud.com>
2018-07-31 13:25:16 +0200

    Test Anfang + UML

---
+++ src/Test/Tests.java
@@ -0,0 +1,15 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.*;
+
+public class Tests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
