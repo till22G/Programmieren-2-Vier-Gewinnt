51b6664899bb59458dcad7357d23fc8ac321b175
gertga <gertga@gertga-1.ger.SAS.com>
2018-08-01 18:23:15 +0200

    Speichern/Laden von Arraylist mit Spielerprofilen funktioniert jetzt

--- src/speichern_laden/Speichern.java
+++ src/speichern_laden/Speichern.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 import main.Spielerprofil;;
 
@@ -10,7 +11,7 @@ public class Speichern {
 		
 			
 	//Spielerprofil  wir methode übergeben damit um dann im Spielerprofil file abgelegt zu werden
-	public static boolean save(Spielerprofil Spieler) {
+	public static boolean save(ArrayList<Spielerprofil> List) {
 		
 		// initialize outputstream out
 		ObjectOutputStream out;
@@ -20,15 +21,16 @@ public class Speichern {
 			//new stream in .txt data
 			out = new ObjectOutputStream(new FileOutputStream("../Programmieren-2-Vier-Gewinnt/src/Spielerprofile/Spielerprofile.txt"));
 			//write Spielerprofil to Spielerprofile.txt
-			out.writeObject(Spieler);
+			out.writeObject(List);
 			//close out
 			out.close();
 			//if write back worked  return true
 			return true;
 		} 
+		
 		catch (IOException e) {
 			//exception message if IOException occurs
-			System.out.println("writing back Spielerprofiel failed");
+			System.out.println("writing back List failed");
 			e.printStackTrace();
 			//if exception is thrown return false
 			return false;
--- src/speichern_laden/SpeichernTest.java
+++ src/speichern_laden/SpeichernTest.java
@@ -1,16 +1,33 @@
 package speichern_laden;
 
+import java.util.ArrayList;
+
+
 import main.Spielerprofil;
 
 public class SpeichernTest {
 	
 	public static void main(String[] args) {
+		  
+		ArrayList<Spielerprofil> L = new ArrayList<>();
 		
+		Spielerprofil s1 = new Spielerprofil("Spast");
+		Spielerprofil s2 = new Spielerprofil("Hurensohn");
+		Spielerprofil s3 = new Spielerprofil("Missgeburt");
+		Spielerprofil s4 = new Spielerprofil("Reagenzglaskind");
 		
-		Spielerprofil s = new Spielerprofil("Hans");
-		Spielerprofil s1 = new Spielerprofil("Horst");
-		Speichern.save(s1);
-		Speichern.save(s);
+		L.add(s1);
+		L.add(s2);
+		L.add(s3);
+		L.add(s4);
+		
+		Speichern.save(L);
+		
+		ArrayList<Spielerprofil> Listööö = Laden.LoadProfiles();
+		
+		for(int i = 0; i < Listööö.size(); i++) {
+			System.out.println(Listööö.get(i).getName());
+		}
 		
 	}
 
