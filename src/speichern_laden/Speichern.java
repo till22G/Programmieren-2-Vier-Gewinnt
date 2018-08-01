1dc9c7c72291d66a048c43471073b4f53dc82ad5
gertga <gertga@gertga-1.ger.SAS.com>
2018-08-01 15:07:39 +0200

    .

--- src/speichern_laden/Speichern.java
+++ src/speichern_laden/Speichern.java
@@ -3,13 +3,6 @@ package speichern_laden;
 
 public class Speichern {
 	
-	//Spielerprofil  wir methode übergeben damit um dann im Spielerprofil file abbgelegt zu werden
-	public static boolean save(Spielerprofil Spieler) {
-		
-		
-		
-		
-		
-	}
+
 
 }
